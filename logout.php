447fdcc61231339f79e7bf32688235b3838672b2
Rahul Kulkarni <rahul.kulkarni1994@gmail.com>
2019-09-29 20:26:33 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Start session
-	session_start();
-//Unset the variables stored in session
-	unset($_SESSION['use']);
-	session_destroy();
-//redirect to login page
-	header('Location:index.html');
-?>
